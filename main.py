7ea6104c7267fcee56dd36e4b6d58f039d011e96
Jakob K <jmaximilliank@gmail.com>
2025-11-22 21:22:37 +0100

    init

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import os
 import sys
 from updater import check_for_updates
 
+#unnötige Fehlermeldung unterdrücken
+os.environ['PYTHONDONTWRITEBYTECODE'] = '1'
+
 # Deine GitHub-Infos hier eintragen
 GITHUB_USER = "Jakob707"
 REPO_NAME = "Papas-Geburtstag"
