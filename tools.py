a0b125b5fd15bafdc032c144acd6a5b92a7f64f9
Jakob Kloiber <jmaximilliank@gmail.com>
2025-11-22 20:59:15 +0100

    Update tools.py

--- tools.py
+++ tools.py
@@ -1,20 +1,44 @@
 def run_tools():
-    print("Verfügbare Tools:")
-    print("1. Beispiel-Tool")
-    print("0. Beenden")
-
-    while True:
-        choice = input("\nAuswahl: ")
-
-        if choice == "1":
-            beispiel_tool()
-        elif choice == "0":
-            print("Tschüss!")
-            break
-        else:
-            print("Ungültige Auswahl")
+    import customtkinter as ctk
 
 
-def beispiel_tool():
-    print("Das ist ein Beispiel-Tool!")
-    # Hier deine Logik
+    ctk.set_appearance_mode("System")
+    ctk.set_default_color_theme('blue')
+
+
+
+    root = ctk.CTk()
+    root.geometry("400x400")
+
+
+    class MainPage(ctk.CTkFrame):
+        def __init__(self, master):
+            super().__init__(master)
+
+            label = ctk.CTkLabel(master=self, text="Welcome!")
+            label.place(relx=0.5, rely=0.2, anchor=ctk.CENTER)
+
+
+
+            def button_function():
+                end_page = EndPage(master)
+                end_page.place(relx=0, rely=0, relwidth=1, relheight=1)
+                end_page.tkraise()
+
+
+            button = ctk.CTkButton(master=self, text="Done", command=button_function)
+            button.place(relx=0.5, rely=0.5, anchor=ctk.CENTER)
+
+
+    class EndPage(ctk.CTkFrame):
+        def __init__(self, master):
+            super().__init__(master)
+
+            self.welcome_text = ctk.CTkLabel(master=self, text=f"Welcome!")
+            self.welcome_text.place(relx=0.5, rely=0.2, anchor=ctk.CENTER)
+
+
+    main_page = MainPage(root)
+    main_page.place(relx=0, rely=0, relwidth=1, relheight=1)
+
+    root.mainloop()
